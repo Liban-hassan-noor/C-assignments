a7380d4c9c5c610c5d1b8b455bbec5742827edae
Liban <libanhassannoor@gmail.com>
2024-10-17 17:07:48 +0300

    surface area and volume

---
+++ surface-area.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Cylinder {
+    public:
+        double pi = 3.14;
+     double radius,height;
+    
+    double surfacearea(){
+       return ( 2 * pi *radius^2 ) *(2 * pi * radius * height); 
+        
+    };
+    
+    double volume(){
+       return pi * radius * height;
+        
+    };
+    
+    
+     
+}:
+
+int main() {
+  Cylinder c1;
+  
+  c1.radius :7 ;
+  c1.height : 21;
+  
+  cout<< "the volume of cylinder is" : <<c1.surfacearea()<<endl;
+  cout<< "the volume of cylinder is" : <<c1.volume()<<endl;
+  
+
+    return 0;
+}
